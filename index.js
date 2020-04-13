e906b164b6f8635a81a4739d66d291acc726db3c
TrinhTrinh00 <trisha.trinh.trinh@gmail.com>
2020-04-12 20:49:23 -0500

    changed value depending on whose turn it is

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+let playerId = 0;
 const containerElem = document.getElementById("container");
 const handleContainerClick = function(e){
     const cellElem = e.target;
-    const dataCell = e.target.attributes[1];
+    const dataCell = e.target.attributes[1];//e.target["data-cell"]
     console.log(dataCell.value);
-    cellElem.innerText = "X";
+    cellElem.innerText = setCellValue();
+    playerId = playerId + 1;
+}
+const setCellValue = function(givenCellElem){
+    if(playerId % 2 == 0){
+        return "X";   
+    }
+    else{
+        return "O";
+    }
 }
 
 containerElem.addEventListener("click", handleContainerClick);
