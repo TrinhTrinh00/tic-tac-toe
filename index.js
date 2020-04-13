cb8fb4764690c7b6838ad05a62b18b0d968b307f
TrinhTrinh00 <trisha.trinh.trinh@gmail.com>
2020-04-12 20:18:24 -0500

    allow cells to change value on click

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const containerElem = document.getElementById("container");
 const handleContainerClick = function(e){
-    console.log(e.target.attributes[1].value);
+    const cellElem = e.target;
+    const dataCell = e.target.attributes[1];
+    console.log(dataCell.value);
+    cellElem.innerText = "X";
 }
 
 containerElem.addEventListener("click", handleContainerClick);
