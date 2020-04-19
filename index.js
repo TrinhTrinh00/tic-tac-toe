1c719ab47761d2e344fc133ff976d28e724bedfa
TrinhTrinh00 <trisha.trinh.trinh@gmail.com>
2020-04-18 19:40:08 -0500

    created rules

--- index.js
+++ index.js
@@ -3,17 +3,47 @@ const containerElem = document.getElementById("container");
 const handleContainerClick = function(e){
     const cellElem = e.target;
     const dataCell = e.target.attributes[1];//e.target["data-cell"]
-    console.log(dataCell.value);
-    cellElem.innerText = setCellValue();
-    playerId = playerId + 1;
+    const isLegalMove = checkLegalMove(cellElem);
+
+    if(isLegalMove){
+        const revisedPlayerSymbol = getPlayerSymbol(cellElem);
+        cellElem.innerText = revisedPlayerSymbol;
+
+        const revisedPlayerId = incrementPlayer(playerId);
+        playerId = revisedPlayerId;
+       
+        updateDisplay(playerId);
+    } 
 }
-const setCellValue = function(givenCellElem){
-    if(playerId % 2 == 0){
-        return "X";   
-    }
-    else{
-        return "O";
-    }
+
+const updateDisplay = function(givenPlayerId){
+    const playerTurnElem = document.getElementById("playerTurn");
+    const revisedCellValue = getPlayerSymbol(givenPlayerId);
+    playerTurnElem.innerText = revisedCellValue + "'s Turn";
 }
+
+const getPlayerSymbol = function(givenPlayerId){
+    if(playerId === 0){
+        return "X";
+    }
+    return "O";
+}
+
+const incrementPlayer = function(givenPlayerId){
+    if(givenPlayerId === 0){
+        return 1;
+    }
+
+    return 0;
+}
+
+const checkLegalMove = function(givenCellElem){
+    const currentValue = givenCellElem.innerText;
+    const isLegal = currentValue === ".";
+   
+    return isLegal;
+}
+
+
 
 containerElem.addEventListener("click", handleContainerClick);
